d7c83ab82cc62e0d9d275f6e97ab78cbf1a3a1d2
Shakyakithmini <shakyakhasangana@gmail.com>
2023-11-21 17:32:07 -0600

    Update new_exercies

---
+++ Exercies/Exercies_02/js/script.js
@@ -0,0 +1,15 @@
+  function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello Java Script !!!";
+  document.getElementById("demo2").innerHTML = "Is it Easy or Different ?";
+}
+function myFunction1() {
+    document.getElementById("demo1").innerHTML = "Are you happy?";
+    document.getElementById("demo2").innerHTML = "Or Not?";
+  }
+  function demo4() { 
+  let x = 5;
+let y = 6;
+let z = x + y;
+  document.getElementById("demo4").innerHTML =
+  "The value of z is: " + z;  
+  }
