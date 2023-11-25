44e185b55a8928371d9660f4fad78fb3e33d6305
Shakyakithmini <shakyakhasangana@gmail.com>
2023-11-24 19:49:26 -0600

    Execies3_update

--- Exercies/Exercies_02/js/script.js
+++ Exercies/Exercies_02/js/script.js
@@ -1,15 +1,36 @@
-  function myFunction() {
+function myFunction() {
   document.getElementById("demo1").innerHTML = "Hello Java Script !!!";
   document.getElementById("demo2").innerHTML = "Is it Easy or Different ?";
 }
 function myFunction1() {
-    document.getElementById("demo1").innerHTML = "Are you happy?";
-    document.getElementById("demo2").innerHTML = "Or Not?";
-  }
-  function demo4() { 
+  document.getElementById("demo1").innerHTML = "Are you happy?";
+  document.getElementById("demo2").innerHTML = "Or Not?";
+}
+function demo4() {
   let x = 5;
-let y = 6;
-let z = x + y;
+  let y = 6;
+  let z = x + y;
   document.getElementById("demo4").innerHTML =
-  "The value of z is: " + z;  
-  }
+    "The value of z is: " + z;
+}
+function demo5() {
+  let x = 10;
+  let y = 6;
+  let z = x - y;
+  document.getElementById("demo5").innerHTML =
+    "The value of z is: " + z;
+}
+function demo6() {
+  let x = 10;
+  let y = 5;
+  let z = x * y;
+  document.getElementById("demo6").innerHTML =
+    "The value of z is: " + z;
+}
+function demo7() {
+  let x = 100;
+  let y = 5;
+  let z = x / y;
+  document.getElementById("demo7").innerHTML =
+    "The value of z is: " + z;
+}
